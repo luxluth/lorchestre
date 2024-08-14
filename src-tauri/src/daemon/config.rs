46008da4b5fdc514425f3a284da5355644f8c178
luxluth <delphin.blehoussi93@gmail.com>
2024-08-14 21:40:32 +0200

    update config (remove usless code)

--- src-tauri/src/daemon/config.rs
+++ src-tauri/src/daemon/config.rs
@@ -1,43 +1,28 @@
 use std::{fs, path::PathBuf};
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const APP_ID: &str = "lorchestre";
+pub const _APP_ID: &str = "lorchestre";
 
 #[derive(Debug, Clone)]
 pub struct Dir {
     pub config: PathBuf,
-    pub app: PathBuf,
     pub cache: PathBuf,
     // pub audio: PathBuf,
 }
 
-pub fn get_dirs() -> Dir {
-    let dir = Dir {
-        config: dirs::config_dir().unwrap().join(APP_ID),
-        app: dirs::data_local_dir().unwrap().join(APP_ID),
-        cache: dirs::cache_dir().unwrap().join(APP_ID),
-        // audio: dirs::audio_dir().unwrap(),
-    };
+impl Dir {
+    pub fn check(&self) {
+        if !self.config.exists() {
+            fs::DirBuilder::new()
+                .recursive(true)
+                .create(&self.config)
+                .unwrap();
+        }
 
-    if !dir.config.exists() {
-        fs::DirBuilder::new()
-            .recursive(true)
-            .create(&dir.config)
-            .unwrap();
+        if !self.cache.exists() {
+            fs::DirBuilder::new()
+                .recursive(true)
+                .create(&self.cache)
+                .unwrap();
+        }
     }
-
-    if !dir.app.exists() {
-        fs::DirBuilder::new()
-            .recursive(true)
-            .create(&dir.app)
-            .unwrap();
-    }
-
-    if !dir.cache.exists() {
-        fs::DirBuilder::new()
-            .recursive(true)
-            .create(&dir.cache)
-            .unwrap();
-    }
-
-    dir
 }
