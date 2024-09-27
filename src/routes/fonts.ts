a4a3148e80b44cf9751dbda5454cd5e44d9b5ac2
luxluth <delphin.blehoussi93@gmail.com>
2024-09-27 16:46:45 +0200

    changing ibm-plex-mono for jetbrains-mo

--- src/routes/fonts.ts
+++ src/routes/fonts.ts
@@ -6,10 +6,5 @@ import '@fontsource-variable/noto-sans-sc';
 import '@fontsource-variable/inter';
 import '@fontsource/calistoga';
 
-import '@fontsource/ibm-plex-mono/100.css';
-import '@fontsource/ibm-plex-mono/200.css';
-import '@fontsource/ibm-plex-mono/300.css';
-import '@fontsource/ibm-plex-mono/400.css';
-import '@fontsource/ibm-plex-mono/500.css';
-import '@fontsource/ibm-plex-mono/600.css';
-import '@fontsource/ibm-plex-mono/700.css';
+// Supports weights 100-800
+import '@fontsource-variable/jetbrains-mono';
