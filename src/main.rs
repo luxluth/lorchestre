302a721341f5bfcf7acb6d80f954445d85bca28a
luxluth <delphin.blehoussi93@gmail.com>
2025-07-09 11:15:15 +0200

    indexing test

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use lorchestre::track::MusicCollection;
+
+fn main() {
+    let mut collection = MusicCollection::default();
+    collection.index("/home/luxluth/Music/".into());
+    eprintln!("{:#?}", collection.artists);
+}
--- src/track.rs
+++ src/track.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     path::PathBuf,
     time::{Duration, SystemTime},
 };
@@ -11,13 +11,14 @@ use lofty::{
     tag::{Accessor, ItemKey},
 };
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub enum Id {
     Digest(md5::Digest),
     Number(usize),
+    Unresolved,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Artist {
     pub id: Id,
     pub name: String,
@@ -30,6 +31,7 @@ pub struct Song {
     pub file_path: PathBuf,
     pub artists: Vec<Id>,
     pub track: u32,
+    pub disc: u32,
     pub album: Option<Id>,
     pub duration: Duration,
     pub bitrate: u32,
@@ -46,6 +48,7 @@ impl Song {
             file_path,
             artists: vec![],
             track: 0,
+            disc: 1,
             album: None,
             duration: Duration::default(),
             bitrate: 0,
@@ -57,13 +60,26 @@ impl Song {
 
 #[derive(Debug)]
 pub struct Album {
-    pub id: usize,
-    pub genres: Vec<String>,
+    pub id: Id,
+    pub genres: HashSet<String>,
     pub artist: Option<Id>,
     pub year: Option<u32>,
     pub songs: Vec<Id>,
     pub disc_total: u32,
     pub songs_count: u32,
+}
+impl Album {
+    fn new(id: Id) -> Self {
+        Self {
+            id,
+            genres: HashSet::new(),
+            artist: None,
+            year: None,
+            songs: vec![],
+            disc_total: 0,
+            songs_count: 0,
+        }
+    }
 }
 
 #[derive(Default)]
@@ -134,10 +150,132 @@ impl MusicCollection {
             if let Some(title) = tag.title() {
                 audio.title = title.to_string();
             }
+
+            if let Some(artists) = tag.get_string(&ItemKey::TrackArtist) {
+                audio.artists = artists
+                    .split(';')
+                    .filter(|x| !x.is_empty())
+                    .map(|x| self.add_artist(x.trim().to_string()))
+                    .collect();
+            };
+
+            audio.disc = tag.disk().unwrap_or(1);
+            if let Some(no) = tag.track() {
+                audio.track = no;
+            }
+
+            if let Some(album) = tag.album() {
+                let mut bytes = album.as_bytes().to_vec();
+                let album_artist = {
+                    if let Some(album_artist) = tag.get_string(&ItemKey::OriginalArtist) {
+                        album_artist
+                    } else {
+                        if let Some(id) = audio.artists.first() {
+                            self.get_artist(id).unwrap().name.as_str()
+                        } else {
+                            "@UNKOWN@"
+                        }
+                    }
+                }
+                .as_bytes();
+                bytes.extend(album_artist);
+
+                let id = self.album_id_store.digest(bytes);
+                if let Some(album) = self.albums.get_mut(&id) {
+                    audio.album = Some(id);
+                    if let Some(genres) = tag.genre() {
+                        if genres.contains(';') {
+                            let genres: Vec<String> =
+                                genres.split(';').map(|x| x.trim().to_string()).collect();
+                            album.genres.extend(genres);
+                        } else {
+                            let genres: Vec<String> =
+                                genres.split(' ').map(|x| x.trim().to_string()).collect();
+                            album.genres.extend(genres);
+                        }
+                    }
+
+                    album.songs.push(audio.id);
+                } else {
+                    // let Album { id, genres, artist, year, songs, disc_total, songs_count }
+                    let mut album = Album::new(id);
+
+                    if let Some(genres) = tag.genre() {
+                        if genres.contains(';') {
+                            let genres: Vec<String> =
+                                genres.split(';').map(|x| x.trim().to_string()).collect();
+                            album.genres.extend(genres);
+                        } else {
+                            let genres: Vec<String> =
+                                genres.split(' ').map(|x| x.trim().to_string()).collect();
+                            album.genres.extend(genres);
+                        }
+                    }
+
+                    album.artist = {
+                        if let Some(album_artist) = tag.get_string(&ItemKey::OriginalArtist) {
+                            self.get_artist_id_by_name(album_artist)
+                        } else {
+                            if let Some(id) = audio.artists.first() {
+                                Some(*id)
+                            } else {
+                                self.get_artist_id_by_name("@UNKOWN@")
+                            }
+                        }
+                    };
+
+                    if let Some(year) = tag.year() {
+                        album.year = Some(year);
+                    } else if let Some(year) = tag.get_string(&ItemKey::Unknown("TDOR".into())) {
+                        album.year = Some(year.parse().unwrap_or(0));
+                    }
+
+                    album.songs.push(audio.id);
+                    if let Some(tt) = tag.track_total() {
+                        album.songs_count = tt;
+                    }
+
+                    album.disc_total = tag.disk_total().unwrap_or(1);
+                }
+            }
         }
     }
 
+    pub fn add_artist(&mut self, name: String) -> Id {
+        for (id, artist) in &self.artists {
+            if artist.name == name {
+                return *id;
+            }
+        }
+
+        let id = self.artist_id_store.next();
+        let artist = Artist { id, name };
+
+        self.artists.insert(id, artist);
+
+        return id;
+    }
+
+    pub fn get_artist_id_by_name(&self, name: &str) -> Option<Id> {
+        for (id, artist) in &self.artists {
+            if artist.name == name {
+                return Some(*id);
+            }
+        }
+
+        None
+    }
+
+    pub fn get_artist(&self, id: &Id) -> Option<&Artist> {
+        self.artists.get(id)
+    }
+
+    pub fn get_album(&self, id: &Id) -> Option<&Album> {
+        self.albums.get(id)
+    }
+
     pub fn index(&mut self, dir_path: PathBuf) {
+        self.add_artist("@UNKOWN@".to_string());
         if let Ok(paths) = glob(&format!("{}/**/*", dir_path.display())) {
             for inode in paths.flatten() {
                 if inode.is_file() {
